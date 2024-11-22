f640bab9455bdbd4cc30ea5960566458df0e97e5
Lord Cigel Salvar <lordcigelsalvar@gmail.com>
2024-11-23 03:33:22 +0800

    implement of notif modal

--- includes/clientnavbar.php
+++ includes/clientnavbar.php
@@ -7,20 +7,51 @@ if (substr($basePath, -1) !== '/') {
     $basePath .= '/';
 }
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="container-fluid">
-        <button class="btn btn-outline-secondary me-2" id="sidebarToggle">☰</button>
-        <a class="navbar-brand" href="#">IntelliDoc</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navbar with Notifications</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e31b23;">
+        <a class="navbar-brand text-white" href="#">INTELLIDOC</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#studentNavbar" aria-controls="studentNavbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link active" href="cleint.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="../public/forms.php">Acitivity Form</a></li>
-                <li class="nav-item"><a class="nav-link" href="notifications">Notifications</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+
+        <div class="collapse navbar-collapse" id="studentNavbar">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="student_dashboard.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="studentActivities.html">Student Activities Forms</a>
+                </li>
+                <li class="nav-item">
+                    <?php include 'notifModal.php'; ?>
+                    <button type="button" class="navbar btn-light" data-toggle="modal" data-target="#notificationModal">
+                        Notification
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="logout.html">Logout</a>
+                </li>
             </ul>
         </div>
-    </div>
-</nav>
+    </nav>
+
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ includes/notifModal.php
@@ -0,0 +1,30 @@
+<!-- Notification Modal -->
+<div class="modal fade" id="notificationModal" tabindex="-1" role="dialog" aria-labelledby="notificationModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="notificationModalLabel">Notifications</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <strong>Activity Proposal Form</strong><br>
+                        Submitted on: 2024-11-15<br>
+                        Status: <span class="badge badge-success">Approved</span>
+                    </li>
+                    <li class="list-group-item">
+                        <strong>Booking Form</strong><br>
+                        Submitted on: 2024-11-11<br>
+                        Status: <span class="badge badge-danger">Disapproved</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
