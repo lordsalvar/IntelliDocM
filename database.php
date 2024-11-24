68ababa4aebf9b2a2793964b98f56c6e8fa242f4
Lord Cigel Salvar <lordcigelsalvar@gmail.com>
2024-11-25 01:55:54 +0800

    db

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "capstone";
+$dbname = "intellidoc";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
