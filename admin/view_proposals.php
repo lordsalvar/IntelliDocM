ec50b7b1382026fc91a94391d86136454a8f0367
jaslup16 <jaslupiba@gmail.com>
2024-11-26 23:22:34 +0800

    updated

--- admin/view_proposals.php
+++ admin/view_proposals.php
@@ -96,6 +96,9 @@ $result = $conn->query($sql);
             </div>
         </div>
     </div>
+
+    <?php include '../includes/footer.php' ?>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
