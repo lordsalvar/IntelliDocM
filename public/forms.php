ee1638e97bb393b14c87d7c263c2b1c2c76620ec
Lord Cigel Salvar <lordcigelsalvar@gmail.com>
2024-12-01 15:13:33 +0800

    update

--- public/forms.php
+++ public/forms.php
@@ -44,44 +44,11 @@ require_once '../database.php';
                 </div>
             </div>
 
-            <!-- Parent Consent Form -->
-            <div class="col-md-4">
-                <div class="card text-center">
-                    <div class="card-body">
-                        <h5 class="card-title">Parent Consent Form</h5>
-                        <p class="card-text">
-                            Get parental consent for activity participation.
-                        </p>
-                        <a
-                            href="parent_consent_form."
-                            class="btn btn-primary">Access Form</a>
-                    </div>
-                </div>
-            </div>
 
-            <!-- Affidavit of Consent Form -->
-            <div class="col-md-4">
-                <div class="card text-center">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            Affidavit of Consent Form
-                        </h5>
-                        <p class="card-text">
-                            Submit your affidavit of consent for activities.
-                        </p>
-                        <a
-                            href="affidavit_consent_form.html"
-                            class="btn btn-primary">Access Form</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+            <!-- Bootstrap JS and dependencies -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 </body>
 <?php include '../includes/footer.php' ?>
 
